7b9fc019651fe7d6850c4850a10b82feed687b22
Dale Alleshouse <dalealleshouse@gmail.com>
2015-12-17 22:04:43 -0500

    missed file

---
+++ TuringMachine.Tests/Tape/CtorShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using AssertExLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TuringMachine.Tests.Tape
+{
+    [TestClass]
+    public class CtorShould
+    {
+        [TestMethod]
+        public void ThrowIfHeadPostionIsInvalid()
+        {
+            AssertEx.Throws<IndexOutOfRangeException>(
+                () => new TuringMachine.Tape(new[] {'a', 'b'}, 3));
+
+            AssertEx.Throws<IndexOutOfRangeException>(
+                () => new TuringMachine.Tape(new[] { 'a', 'b' }, -1));
+        }
+    }
+}
